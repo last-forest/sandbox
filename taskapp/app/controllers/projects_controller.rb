6eef331ec68ffb6179fbf1d7f39d071e76cea012
shagiocom <satoru.morimoto.9x@gmail.com>
2014-09-25 00:57:42 +0900

    update

--- taskapp/app/controllers/projects_controller.rb
+++ taskapp/app/controllers/projects_controller.rb
@@ -6,4 +6,26 @@ class ProjectsController < ApplicationController
     def show
         @project = Project.find(params[:id])
     end
+
+    def new 
+        @project = Project.new
+    end
+
+    def create 
+        @project = Project.new(project_params)
+        if @project.save
+            redirect_to projects_path
+        else
+            render 'new'
+      t end
+    end
+
+    private
+
+        def project_params
+            params[:project].permit(:title)
+        end
+
+
+
 end
