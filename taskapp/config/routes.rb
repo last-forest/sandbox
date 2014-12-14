c04a07b837305ab8fd5686593d6823a871d3d376
shagiocom <satoru.morimoto.9x@gmail.com>
2014-12-15 00:57:03 +0900

    update

--- taskapp/config/routes.rb
+++ taskapp/config/routes.rb
@@ -3,11 +3,6 @@ Rails.application.routes.draw do
     resources :tasks, only: [:create, :destroy]
   end
 
-
-
-
-
-
   root 'projects#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
